5635025352f2eebc72833f583f0b7fd73934e854
mojyack <mojyack@gmail.com>
2023-02-24 18:24:43 +0900

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include "imgview.hpp"
-#include "path.hpp"
 
 int main(const int argc, const char* const argv[]) {
     if(argc != 2) {
         return 0;
     }
+
     auto app = Gawl::Application();
     app.open_window<Imgview>({.title = "Imgview", .manual_refresh = true}, argv[1]);
     app.run();
